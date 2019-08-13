fb48739e283bf5ecd93920690e041100a093f96c
Jonesh Shrestha <contact@joneshshrestha.com>
2019-08-13 09:21:10 +0545

    update signup.html

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//jshint esversion: 6
+
 let express = require('express')
 let request = require('request')
 let bodyParser = require('body-parser')
@@ -5,6 +7,9 @@ let bodyParser = require('body-parser')
 let app = express()
 
 app.use(bodyParser.urlencoded({extended: true}))
+
+
+
 
 app.listen(3000, function(){
     console.log("Listening at PORT 3000")
