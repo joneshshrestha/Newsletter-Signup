7a4ae77de7b50214e4c321081c03fa8637483170
Jonesh Shrestha <contact@joneshshrestha.com>
2019-08-13 09:52:22 +0545

    update express.static

--- app.js
+++ app.js
@@ -8,7 +8,11 @@ let app = express()
 
 app.use(bodyParser.urlencoded({extended: true}))
 
+app.use(express.static('public'))
 
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/signup.html')
+})
 
 
 app.listen(3000, function(){
