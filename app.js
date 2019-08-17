9a6012ec34ba3491eaffa2ab71c823a4ec209407
Jonesh Shrestha <contact@joneshshrestha.com>
2019-08-17 12:00:16 +0545

    update

--- app.js
+++ app.js
@@ -7,11 +7,18 @@ let bodyParser = require('body-parser')
 let app = express()
 
 app.use(bodyParser.urlencoded({extended: true}))
-
 app.use(express.static('public'))
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/signup.html')
+})
+
+app.post('/', function(req, res){
+    let firstName = req.body.fName
+    let lastName = req.body.lName
+    let email = req.body.email
+
+    console.log(firstName, lastName, email)
 })
 
 
