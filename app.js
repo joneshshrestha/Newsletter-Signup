712787a267f260a1fd8c8db0fe9ecef8b416514b
Jonesh Shrestha <contact@joneshshrestha.com>
2019-08-18 14:14:53 +0545

    mailchip server

--- app.js
+++ app.js
@@ -19,9 +19,27 @@ app.post('/', function(req, res){
     let email = req.body.email
 
     console.log(firstName, lastName, email)
+
+    let option = {
+        url: 'https://us3.api.mailchimp.com/3.0/lists/f68a58c0cf',
+        method: 'POST',
+
+    }
+
+    request(option, function(error, response, body) {
+        if(error) {
+            console.log(error)
+        } else {
+            console.log(response.statusCode)
+        }
+    })
 })
 
 
-app.listen(3000, function(){
+app.listen(3001, function(){
     console.log("Listening at PORT 3000")
 })
+
+//API KEY
+//b8f9e122cea0c5e6ad7605807087a8a2-us3
+//f68a58c0cf
